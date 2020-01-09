0a9f7cc0a7e1395686e2c9dee12e475d1d30e0bf
andrewmathies <andrew.mathies@geappliances.com>
2020-01-09 14:47:14 -0500

    mistook e for err, it is event

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -5,12 +5,7 @@ req.send();
 
 let viList
 
-req.onreadystatechange = (err) => {
-    if (err) {
-        console.log('request for VI data failed', err)
-        return
-    }
-
+req.onreadystatechange = (event) => {
     if(req.readyState !== XMLHttpRequest.DONE && req.status !== 200) {
         console.log('request for VI data was not successful', req.status)
         return
