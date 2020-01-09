ac7f0001b548bff3d59a8946b727d71f17e47ac4
andrewmathies <andrew.mathies@geappliances.com>
2020-01-09 14:31:27 -0500

    adding doc page to frontend, starting search js

--- frontend/js/doc.js
+++ frontend/js/doc.js
@@ -1,16 +1,14 @@
+
+/*
 const Http = new XMLHttpRequest();
 const url='http://saturten.com/api/vi';
 Http.open("GET", url);
 Http.send();
-
-let viList
+console.log('sending request')
 
 Http.onreadystatechange = (err) => {
   let response = Http.responseText
+  let viList = JSON.parse(response)
   console.log(response)
-  viList = JSON.parse(response)
-  viList.sort((a, b) =>  { return a.Name < b.Name })
-
-  console.log('sorted list?')
-  console.log(viList)
 }
+*/
--- index.js
+++ index.js
@@ -6,15 +6,10 @@ const app = express()
 const port = 80
 
 app.use(express.json())
+app.use(express.static('frontend'))
 
 app.get('/api/vi', db.getVis)
 app.post('/api/vi', db.createVis)
-
-app.use(express.static('frontend'))
-
-app.get('/doc', (req, res) => {
-	res.sendFile('/home/ec2-user/labview_docs/frontend/doc.html')
-})
 
 app.listen(port, () => {
 	console.log('starting server on port ' + port)
